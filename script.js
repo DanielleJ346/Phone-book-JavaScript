68e44121f126a552c00508856cc8b48c0138a309
DanielleJ346 <djormerod1@gmail.com>
2023-08-20 09:36:44 +0200

    new

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Listen to form submit
 document.getElementById('contactForm').addEventListener('submit', contactForm);
 
+// validating phone numbers
+var personMobileInput = document.getElementById('personMobile');
+personMobileInput.addEventListener('input', validatePhoneNumber);
+
 // save contact
 function contactForm(e) {
     // get values from form
@@ -8,6 +12,13 @@ function contactForm(e) {
     var surname = document.getElementById('surname').value;
     var personMobile = document.getElementById('personMobile').value;
     
+     // Validate the phone number format
+     var phoneNumberPattern = /^\d{3}-\d{3}-\d{4}$/;
+     if (!phoneNumberPattern.test(personMobile)) {
+         alert("Invalid phone number format. Please use the format: 081-365-9535");
+         return false; // Prevent form submission
+     }
+
     if(!personName || !personMobile) {
         alert("Please fill in all fields");
         return false;
@@ -35,7 +46,7 @@ function contactForm(e) {
         // first get contacts from local storage
         var phonebook = JSON.parse(localStorage.getItem('phonebook'));
         //add new contact to array
-        phonebook.push(contacts);
+        phonebook.push(contact);
         //rest back to local storage
         localStorage.setItem('phonebook', JSON.stringify(phonebook));
     }
@@ -49,57 +60,92 @@ function contactForm(e) {
 }
 
 // fetch the contacts from localStorage
-
 function fetchPhonebook() {
-     // first get contacts from local storage
-     var phonebook = JSON.parse(localStorage.getItem('phonebook'));
+    // Get contacts from local storage
+    var phonebook = JSON.parse(localStorage.getItem('phonebook'));
 
-     // get the output ID
-     var phonebookResult = document.getElementById('phonebookResult');
+    // Sort the phonebook array alphabetically based on the name
+    phonebook.sort(function(a, b) {
+        var nameA = a.name.toLowerCase();
+        var nameB = b.name.toLowerCase();
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+        return 0;
+    });
 
-     phonebookResult.innerHTML = '';
+    // Get the output ID
+    var phonebookResult = document.getElementById('phonebookResult');
 
-     for (var i = 0; i < phonebook.length; i++) {
+    phonebookResult.innerHTML = '';
+
+    for (var i = 0; i < phonebook.length; i++) {
         var name = phonebook[i].name;
         var surname = phonebook[i].surname;
         var mobile = phonebook[i].mobile;
 
         phonebookResult.innerHTML += '<tr>'+
-                                        '<td class="contact-name">'+name+'</td>'+
-                                        '<td class="contact-surname">'+surname+'</td>'
-                                        '<td class="contact-number">'+mobile+'</td>'
-                                        '<td><a href="#" class="btn btn-danger" onclick="deleteContact('+mobile+')">Delete</a></td>'+
-                                        '</tr>';                    
-     }
+                                    '<td class="contact-name">'+name+'</td>'+
+                                    '<td class="contact-surname">'+surname+'</td>' +
+                                    '<td class="contact-number">'+mobile+'</td>' +
+                                    '<td><a href="#" class="btn btn-danger" onclick="deleteContact(\''+mobile+'\')">Delete</a></td>'+
+                                    '</tr>';
+    }
 }
 
-// JavaScript for search bar
+
+// search bar
 function search_contact() {
-	let input = document.getElementById('searchbar').value
-	input=input.toLowerCase();
-	let x = document.getElementsByClassName('contact-name');
-	
-	for (i = 0; i < x.length; i++) {
-		if (!x[i].innerHTML.toLowerCase().includes(input)) {
-			x[i].style.display="none";
-		}
-		else {
-			x[i].style.display="list-item";				
-		}
-	}
-}
+    let input = document.getElementById('searchbar').value.toLowerCase();
+    let rows = document.getElementsByClassName('contact-row');
 
-function deleteContact(num) {
-    var phonebook = JSON.parse(localStorage.getItem('phonebook'));
-    for (var i = 0; i < phonebook.length; i++) {
-        if ((phonebook[i].mobile == num)) {
-            phonebook.splice(i, 1);
+    for (let i = 0; i < rows.length; i++) {
+        let name = rows[i].querySelector('.contact-name').textContent.toLowerCase();
+        let surname = rows[i].querySelector('.contact-surname').textContent.toLowerCase();
+
+        if (name.includes(input) || surname.includes(input)) {
+            rows[i].classList.remove('hidden');
+        } else {
+            rows[i].classList.add('hidden');
         }
     }
-    // reset back to localStorage
-    localStorage.setItem("phonebook",JSON.stringify(phonebook));
-
-    fetchPhonebook();
 }
 
-//get the search bar to work
+
+//delete contacts
+function deleteContact(num) {
+    var phonebook = JSON.parse(localStorage.getItem('phonebook'));
+    
+    var indexToDelete = -1;
+
+    for (var i = 0; i < phonebook.length; i++) {
+        if (phonebook[i].mobile == num) {
+            indexToDelete = i;
+            break; // No need to continue searching
+        }
+    }
+
+    if (indexToDelete !== -1) {
+        phonebook.splice(indexToDelete, 1);
+
+        // reset back to localStorage
+        localStorage.setItem("phonebook", JSON.stringify(phonebook));
+
+        fetchPhonebook();
+    }
+}
+
+// validating phone numbers
+function validatePhoneNumber() {
+    var phoneNumber = this.value; // Get the entered phone number
+    var phoneNumberPattern = /^\d{3}-\d{3}-\d{4}$/;
+
+    // Test if the entered phone number matches the pattern
+    if (phoneNumberPattern.test(phoneNumber)) {
+        this.classList.remove('invalid');
+        this.classList.add('valid');
+    } else {
+        this.classList.remove('valid');
+        this.classList.add('invalid');
+    }
+}
+
